0c267da19cbc23c2dfa19dbf9a3f7a5af63f1087
onylenie <lapaev0202@mail.ru>
2024-12-15 03:40:45 +0300

    Done lab9

--- pythonProject/models.py
+++ pythonProject/models.py
@@ -8,8 +8,8 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    username = Column(String(100), unique=True, nullable=False)
-    email = Column(String(150), unique=True, nullable=False)
+    username = Column(String(50), unique=True, nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
     password = Column(String(100), nullable=False)
 
     posts = relationship('Post', back_populates='user', cascade="all, delete")
@@ -19,8 +19,8 @@ class Post(Base):
     __tablename__ = 'posts'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    title = Column(String(200), nullable=False)
+    title = Column(String(100), nullable=False)
     content = Column(Text, nullable=False)
-    user_id = Column(Integer, ForeignKey('users.id', ondelete="CASCADE"))
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
 
     user = relationship('User', back_populates='posts')
